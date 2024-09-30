46a38143b7a749ef978377a00c1f5494c6dfe15a
simuseokyun <sim31059999@gmail.com>
2024-09-30 16:47:47 +0900

    feat : 구조 잡기

---
+++ src/app/(afterlogin)/home/layout.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function HomeLayout({ children }: { children: React.ReactNode }) {
+    return <div>{children}홈 레이아ㅜㅅ</div>;
+}
---
+++ src/app/not-found.tsx
@@ -0,0 +1,10 @@
+import Image from 'next/image';
+const NotFound = () => {
+    return (
+        <div>
+            <h1>Page Not Found</h1>
+        </div>
+    );
+};
+
+export default NotFound;
