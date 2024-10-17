870b7ebdf4fa73edba0bf291d819d3704f33b67a
simuseokyun <sim31059999@gmail.com>
2024-10-17 19:37:51 +0900

    feat : 기능 추가 및 디자인

--- src/app/(afterlogin)/home/page.tsx
+++ src/app/(afterlogin)/home/page.tsx
@@ -1,3 +1,27 @@
+import style from './home.module.css';
+import Tab from '@/app/(afterlogin)/home/_component/Tab';
+import PostForm from '@/app/(afterlogin)/home/_component/PostForm';
+import Post from '@/app/(afterlogin)/_component/Post';
+import TabProvider from './_component/TabProvider';
 export default function Home() {
-    return <main>홈페이지</main>;
+    return (
+        <main className={style.main}>
+            <TabProvider>
+                <Tab />
+                <PostForm />
+                <Post />
+                <Post />
+                <Post />
+                <Post />
+                <Post />
+                <Post />
+                <Post />
+                <Post />
+                <Post />
+                <Post />
+                <Post />
+                <Post />
+            </TabProvider>
+        </main>
+    );
 }
--- src/app/(afterlogin)/layout.tsx
+++ src/app/(afterlogin)/layout.tsx
@@ -5,6 +5,8 @@ import Image from 'next/image';
 import ZLogo from '../../../public/zlogo.png';
 import NavMenu from './_component/NavMenu';
 import LogoutButton from './_component/LogoutButton';
+import TrendSection from './_component/TrendSection';
+import FollowRecommend from './_component/FollowRecommend';
 
 export default function AfterLoginLayout({ children }: { children: React.ReactNode }) {
     return (
@@ -43,10 +45,13 @@ export default function AfterLoginLayout({ children }: { children: React.ReactNo
                                 <input type="search" />
                             </form>
                         </div>
-
+                        <TrendSection />
                         <div className={style.followRecommend}>
                             <h3>팔로우 추천</h3>
                         </div>
+                        <FollowRecommend />
+                        <FollowRecommend />
+                        <FollowRecommend />
                     </section>
                 </div>
             </div>
